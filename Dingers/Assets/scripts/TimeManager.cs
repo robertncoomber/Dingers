70a8da81dd8eea277c4ec387e16bbd4e11b24eb4
Robert Coomber <robertnc@gmail.com>
2019-02-03 22:23:13 -0600

    First Commit With Field and New Menu
    The Menu works with current placeholders
    
    Because I left VRTK the fixedjoint rigidbody is
    causing issues where the bat is moving through the
    ball. I need to figure that out.

---
+++ Dingers/Assets/scripts/TimeManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+	public Text timeDisplay;
+	private float timer =  121f;
+	private string minutes, seconds;
+	private bool isPaused = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( !isPaused )
+		{
+		timer -= Time.deltaTime;
+		minutes = Mathf.Floor(timer / 60).ToString("0");
+		seconds = Mathf.Floor(timer % 60).ToString("00");
+		timeDisplay.text = minutes + ":" + seconds;
+		//Debug.Log(minutes + seconds + timer);
+		}
+
+		if (timer <= 0 && !isPaused) {
+			timeDisplay.text = "0:00";
+		}
+
+	}
+
+	public void PlayGame()
+	{
+		isPaused = false;
+	}
+
+	public void PauseGame()
+	{
+		isPaused = true;
+	}
+
+	public void RestartTimer()
+	{
+		timer = 121;
+		isPaused = false;
+	}
+}
---
+++ Dingers/Assets/scripts/scoreManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class scoreManager : MonoBehaviour {
+
+	public Text score;
+	public static int scoreNum;
+
+	void Awake()
+	{
+		score = GetComponent<Text> ();
+		scoreNum = 0;
+	}
+
+	void Update () {
+		score.text = scoreNum.ToString();
+	}
+
+
+	public void RestartScore()
+	{
+		scoreNum = 0;
+	}
+
+
+}
