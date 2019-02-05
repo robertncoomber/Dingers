0427deedcdd13e8ff463283a9c5f5b2cb8205f79
Robert Coomber <robertnc@gmail.com>
2019-02-04 23:14:46 -0600

    Fixed Baseball Bat Issues
    Only start with bat, can change material.
    
    No dropping bat rn

---
+++ Dingers/Assets/scripts/Bat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour {
+
+    public List<Material> bats;
+    Material batMat;
+    int i = 0;
+    int numBats;
+
+    GameObject bat;
+
+    private void Start()
+    {
+        bat = gameObject;
+        numBats = bats.Count;
+        batMat = GetComponent<Renderer>().material;
+    }
+
+    public void ChangeBat()
+    {
+        if(i == (numBats-1))
+        {
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+
+        bat.GetComponent<Renderer>().material = bats[i];
+        batMat = bats[i];
+        
+    }
+}
