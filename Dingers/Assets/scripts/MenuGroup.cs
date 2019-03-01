081fe6a7b1a20506c33fec3ab85a2730d3283112
Robert Coomber <robertnc@gmail.com>
2019-03-01 14:51:44 -0600

    Menu navigable animtions working. Functions still not mapped properly.

---
+++ Dingers/Assets/scripts/MenuGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rob.VR
+{
+    public class MenuGroup : MonoBehaviour
+    {
+        public int menuID;
+        public List<IP_VR_MenuButton> menuButtons = new List<IP_VR_MenuButton>();
+    }
+}
