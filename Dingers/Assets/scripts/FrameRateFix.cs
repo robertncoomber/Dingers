33cd1013c8aee07b25867a7bf6b63e899571306b
Robert Coomber <robertnc@gmail.com>
2019-03-22 15:55:05 -0500

    vrtk menu start

---
+++ Dingers/Assets/scripts/FrameRateFix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (UnityEditorInternal.ProfilerDriver.firstFrameIndex > 0)
+        {
+            UnityEditorInternal.ProfilerDriver.ClearAllFrames();
+        }
+    }
+}
